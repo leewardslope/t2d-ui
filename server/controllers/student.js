13b201b9b1769f95eba8f717193531f39a971fe2
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2021-08-15 03:32:27 +0000

    Configured Controller for Router

---
+++ server/controllers/student.js
@@ -0,0 +1,3 @@
+export const getStudents = (req, res) => {
+  res.send('Router is Working');
+};
--- server/routes/student.js
+++ server/routes/student.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { getStudents } from '../controllers/student.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Router is Working');
-});
+router.get('/', getStudents);
 
 export default router;
