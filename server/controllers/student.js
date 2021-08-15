96099e438a080713b5337c0a57b87a315f8c85f4
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2021-08-15 04:29:00 +0000

    server side create-new-student setup

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -1,11 +1,7 @@
 // Best place to list down all the router callbacks.
 
-export const getStudents = (req, res) => {
-  res.send('Router is Working');
-};
-
 // We will create a try catch block, because we are getting a request from the client and we need to handle that. => We need error handling
-export const createStudent = async (req, res) => {
+export const getStudents = async (req, res) => {
   try {
     // It will go to the student model and save its in this variable.
     const allStudents = await student.find();
@@ -13,5 +9,19 @@ export const createStudent = async (req, res) => {
     res.status(200).json(allStudents);
   } catch (error) {
     res.status(404).json({ message: error.message });
+  }
+};
+
+export const createStudent = async (req, res) => {
+  const student = req.body;
+  // Creating new student via model
+  const newStudent = new student(student);
+
+  try {
+    await newStudent.save();
+
+    res.status(201).json(newStudent);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
   }
 };
