560aa28209c130541fae89628f753022ccad0ee2
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2021-08-20 16:43:30 +0000

    error handeling

--- server/app.js
+++ server/app.js
@@ -7,4 +7,17 @@ const app = express();
 
 app.use('/api/apps', appsRoutes);
 
+// 4 variable middleware, express detects it as a special middle ware to handle, errors.
+// Any error from the above routes will sit here!
+// This should be triggered
+app.use((error, req, res, next) => {
+  // If by chance I had some on-place error handle, it will not repeat again.
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  // I can add this json as end to the above status
+  res.json({ message: error.message || 'An Unknown Error Occurred!' });
+});
+
 app.listen(5000);
--- server/routes/apps-routes.js
+++ server/routes/apps-routes.js
@@ -26,20 +26,35 @@ const DUMMY_APPS = [
   },
 ];
 
+router.get('/:aid', (req, res, next) => {
+  const appId = req.params.aid;
+  const apps = DUMMY_APPS.find(e => {
+    return e.id === appId;
+  });
+
+  // If we return here, the next code will not run. or else create an else statement.
+  if (!apps) {
+    const error = new Error('could not find an app for the app id');
+    error.code = 404;
+    throw error;
+  }
+
+  res.json({ apps });
+});
+
 router.get('/user/:uid', (req, res, next) => {
   const userId = req.params.uid;
   const apps = DUMMY_APPS.filter(e => {
     return e.creator === userId;
   });
 
-  res.json({ apps });
-});
-
-router.get('/:aid', (req, res, next) => {
-  const appId = req.params.aid;
-  const apps = DUMMY_APPS.find(e => {
-    return e.id === appId;
-  });
+  // For some reason, filter method cannot use !apps to give error
+  if (apps.length === 0) {
+    const error = new Error('could not find an app for the user id');
+    error.code = 404;
+    return next(error);
+    // throw error;
+  }
 
   res.json({ apps });
 });
