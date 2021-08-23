466011ef2d5a6356c863f9ffc4f8b7e45608974b
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2021-08-23 13:33:20 +0000

    Automatic login and logout based on expiration

--- client/src/App.js
+++ client/src/App.js
@@ -28,43 +28,70 @@ import Auth from './user/pages/Auth';
 
 import { AuthContext } from './shared/context/auth-context';
 
+let logoutTimer;
+
 function App() {
   const [token, setToken] = useState(null);
   const [userId, setUserId] = useState(false);
-
-
+  const [tokenExpirationDate, setTokenExpirationDate] = useState(); // this variable is differnt from the below used, remember scoping => but using this in it.
 
   // Let's store token in localstorage, i'm not ready for using cookies now!
   const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
-    
-    // need of token expration checks => date + 1hr
-    // either we have it or create it (have it, is in case of 'refresh' login)
-    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60)
-    // Localstorage is gloabally available varibale and it stores in txt format.
-    localStorage.setItem('userData', JSON.stringify({
-      userId: uid, 
-      token: token, 
-      expiration: tokenExpirationDate.toISOString() 
-    }));
 
+    // need of token expiration checks => date + 1hr
+    // either we have it or create it (have it, is in case of 'refresh' login)
+    // 1000 * 60 * 60;
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    // Local storage is globally available variable and it stores in txt format.
+
+    setTokenExpirationDate(tokenExpirationDate);
+
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString(),
+      })
+    );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId(null);
-    localStorage.removeItem('userData')
+    localStorage.removeItem('userData');
   }, []);
 
   useEffect(() => {
-   // Local storage is just a text, we neet to convert it into an object
-    const storedData = JSON.parse(localStorage.getItem('userData'))
-    // Checking if +1 hr time > present time
-    if ( storedData && storedData.token && new Date(storedData.expiration > new Date())) {
-      login(storedData.userId, storedData.token, new Date(storedData.expiration))
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
     }
-  }, [login])
+  }, [token, logout, tokenExpirationDate]);
+
+  useEffect(() => {
+    // Local storage is just a text, we neet to convert it into an object
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    // Checking if +1 hr time > present time
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration > new Date())
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
+    }
+  }, [login]);
 
   let routes;
 
