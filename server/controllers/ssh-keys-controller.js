35c3d202b24333bb75bcfbd1b06b57b1edbd4746
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2021-08-25 05:12:06 +0000

    deleting key id from user when deleting key

--- server/controllers/ssh-keys-controller.js
+++ server/controllers/ssh-keys-controller.js
@@ -1,17 +1,18 @@
 import { validationResult } from 'express-validator';
 import mongoose from 'mongoose';
 import Cryptr from 'cryptr';
-// import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
 
 import HttpError from '../models/https-error.js';
-import SSH from '../models/ssh-schema.js';
+import Ssh from '../models/ssh-schema.js';
 import User from '../models/user-schema.js';
 
-const encrypt = new Cryptr('iwillhack');
+dotenv.config();
+const encrypt = new Cryptr(process.env.CRYPTR_SECRET);
 
 export const getKeys = async (req, res, next) => {
   try {
-    const config = await SSH.findById(req.userData.userId);
+    const config = await Ssh.findById(req.userData.userId);
     res.json(config);
   } catch (error) {
     console.log(error);
@@ -42,7 +43,7 @@ export const postKeys = async (req, res, next) => {
 
   const encryptedString = encrypt.encrypt(keys.identity);
 
-  let newKey = new SSH({
+  let newKey = new Ssh({
     ...keys,
     identity: encryptedString,
     creator: req.userData.userId,
@@ -67,18 +68,48 @@ export const postKeys = async (req, res, next) => {
 
 export const updateKeys = async (req, res, next) => {
   try {
-    const config = await SSH.findById(req.userData.userId);
+    const config = await Ssh.findById(req.userData.userId);
     res.json(config);
   } catch (error) {
     console.log(error);
   }
 };
 
+// The frontend was not ready to use this.
 export const deleteKeys = async (req, res, next) => {
+  // I need to find the id of the key, it will be sent from frontend
+  const keyId = req.params.kid;
+
+  let key;
   try {
-    const config = await SSH.findById(req.userData.userId);
-    res.json(config);
-  } catch (error) {
-    console.log(error);
+    key = await Ssh.findById(keyId).populate('creator'); // in order to use populate we need to configure ref first
+  } catch (err) {
+    return next(
+      new HttpError('Could not delete the key, please try again!', 500)
+    );
   }
+
+  if (!key) {
+    return next(new HttpError('Could not find key for this ID!', 404));
+  }
+
+  // remember => creator here is a full object which has access via populated content using ref
+  if (key.creator.id !== req.userData.userId) {
+    new HttpError('You are not allowed to delete/update this key', 403);
+  }
+
+  try {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    await key.remove({ session: session });
+
+    key.creator.keys.pull(key); // Mongodb Pull => Not Array Pull
+
+    await key.creator.save({ session: session });
+    await session.commitTransaction();
+  } catch (err) {
+    res.status(505).json({ message: err.message });
+  }
+  res.status(200).json({ message: 'key deleted' });
 };
